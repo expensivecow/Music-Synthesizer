6995e72ed8bbd05e4d6a8e838c6e783c6770920f
loriter <mingalinglee@gmail.com>
2015-02-10 08:47:48 -0800

    Refactored opening SD out of getTxtFiles

--- SDRead.c
+++ SDRead.c
@@ -8,75 +8,70 @@ char **list_of_txt;
 alt_up_sd_card_dev *device_reference = NULL;
 short int songFile;
 
+void openSD(void)
+{
+	alt_up_sd_card_dev *device_reference = NULL;
+	int connected = 0;
+	device_reference = alt_up_sd_card_open_dev("/dev/Interface");
+	if (device_reference != NULL) {
+		if ((connected == 0) && (alt_up_sd_card_is_Present())) {
+			printf("Card connected.\n");
+			if (alt_up_sd_card_is_FAT16()) {
+				printf("FAT16 file system detected.\n");
+			} else {
+				printf("Unknown file system.\n");
+			}
+		}
+	}
+}
+
 //Scans files in SD card
 //Gets TXT files and puts into list_of_txt
 void getTxtFiles (void)
 {
-	int connected = 0;
 	short int file_found = 0;
 	char *file_name;
 	int count = 0;
 
-	device_reference = alt_up_sd_card_open_dev(ALTERA_UP_SD_CARD_AVALON_INTERFACE_0_NAME);
-	if (device_reference != NULL)
+	//Gets number of txt files
+	file_found = alt_up_sd_card_find_first ("//", file_name);
+	while (file_found == 0)
 	{
-		if ((connected == 0) && (alt_up_sd_card_is_Present()))
+		file_found = alt_up_sd_card_find_next(file_name);
+		if (file_found == 0)
 		{
-			printf("Card connected.\n");
-			if (alt_up_sd_card_is_FAT16())
-				printf("FAT16 file system detected.\n");
-			else
-				printf("Unknown file system.\n");
-			connected = 1;
-
-			//Gets number of txt files
-			file_found = alt_up_sd_card_find_first ("//", file_name);
-			while (file_found == 0)
+			printf ("%s\n", file_name);
+			int length = strlen(file_name);
+			char* extension = file_name[length - 4];
+			if(strcmp(extension, ".txt")
 			{
-				file_found = alt_up_sd_card_find_next(file_name);
-				if (file_found == 0)
-				{
-					exist = 1;
-					printf ("%s\n", file_name);
-					int length = strlen(file_name);
-					char* extension = file_name[length - 4];
-					if(strcmp(extension, ".txt")
-					{
-						num_of_txt++;
-					}
-				}
-				else if (file_found == 1)
-				{
-					printf ("Invalid directory.\n");
-				}
-			}
-			//Allocates memory for txt files
-			list_of_txt = malloc(num_of_txt * sizeof(char*));
-
-			//Places names of txt files into array
-			file_found = alt_up_sd_card_find_first ("//", file_name);
-			while (file_found == 0)
-			{
-				file_found = alt_up_sd_card_find_next(file_name);
-				if (file_found == 0)
-				{
-					int length = strlen(file_name);
-					char* extension = file_name[length - 4];
-					if(strcmp(extension, ".txt")
-					{
-						list_of_txt[count] = file_name;
-						count++;
-					}
-				}
+				num_of_txt++;
 			}
 		}
-		else if ((connected == 1) && (alt_up_sd_card_is_Present() == false))
+		else if (file_found == 1)
 		{
-			printf("Card disconnected.\n");
-			connected = 0;
+			printf ("Invalid directory.\n");
 		}
 	}
-	//printf ("Error, please check card.\n");
+	//Allocates memory for txt files
+	list_of_txt = malloc(num_of_txt * sizeof(char*));
+
+	//Places names of txt files into array
+	file_found = alt_up_sd_card_find_first ("//", file_name);
+	while (file_found == 0)
+	{
+		file_found = alt_up_sd_card_find_next(file_name);
+		if (file_found == 0)
+		{
+			int length = strlen(file_name);
+			char* extension = file_name[length - 4];
+			if(strcmp(extension, ".txt")
+			{
+				list_of_txt[count] = file_name;
+				count++;
+			}
+		}
+	}
 }
 
 void displayTxtFiles (void)
@@ -99,4 +94,11 @@ int** readTxtFile (char* filename)
 	}while(letter != -1);
 	
 	return contents;
+}
+
+void main(void)
+{
+	openSD();
+	getTxtFiles();
+	displayTxtFiles();
 }
